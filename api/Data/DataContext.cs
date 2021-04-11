86063293bf27cc4312121d63595143488484b5d1
Son Vu <vuthaison198x@gmail.com>
2021-04-11 21:41:27 +0700

    init api

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        
+    }
+}
