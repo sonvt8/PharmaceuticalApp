d234ccaf69cec329c17e397b3023e161ae98d681
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-04-12 13:29:15 +0700

    Configuring cloudinary in the API

--- api/Startup.cs
+++ api/Startup.cs
@@ -32,6 +32,7 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CloudinarySettings>(_config.GetSection("CloudinarySettings"));
             services.AddCors();
             services.AddControllers();
             services.AddDbContext<DataContext>(options =>
