085a963d746f900f26d3f51746aae63e085355e1
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-04-29 15:18:18 +0700

    Finish Category Admin

--- admin/src/app/_service/category.service.ts
+++ admin/src/app/_service/category.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Category } from '../_models/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  baseUrl = environment.apiUrl + "/categories";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  postCategory(val: Category){
+    return this.http.post(this.baseUrl,val);
+  }
+
+  putCategory(val: Category){
+    return this.http.put(`${this.baseUrl}/${val.id}`,val);
+  }
+
+  deleteCategory(val: Category){
+    return this.http.delete(`${this.baseUrl}/${val.id}`);
+  }
+
+  resetList(){
+    return this.http.get(this.baseUrl);
+  }
 }
--- admin/src/app/admin/category-admin/add-edit-category/add-edit-category.component.ts
+++ admin/src/app/admin/category-admin/add-edit-category/add-edit-category.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Category } from 'src/app/_models/category';
+import { CategoryService } from 'src/app/_service/category.service';
 
 @Component({
   selector: 'app-add-edit-category',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditCategoryComponent implements OnInit {
 
-  constructor() { }
+  @Input() cate: Category;
+  @Input()
+  public myCallback: Function;
+  ModalTitle: string
+
+  constructor(public categoryService: CategoryService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+  onSubmit() {
+    if (this.cate.id == 0) {
+      this.insertRecord();
+    }
+    else {
+      this.updateRecord();
+    }
+  }
+
+  insertRecord() {
+    this.categoryService.postCategory(this.cate).subscribe(
+      res => {
+        this.myCallback();
+        this.toastr.success('Added successfully');
+      },
+      err => { console.log(err); }
+    )
+  }
+
+  updateRecord() {
+    this.categoryService.putCategory(this.cate).subscribe(
+      res => {
+        this.myCallback();
+        this.toastr.info('Updated successfully');
+      },
+      err => { console.log(err); }
+    )
   }
 
 }
