4fc007a89a5a91fe9242e91bc8d8547bba1fd478
Son Vu <ly.thl2505@sinhvien.hoasen.edu.vn>
2021-05-14 09:01:18 +0700

    reactive job form

--- client/src/app/jobs/job-detail/job-detail.component.ts
+++ client/src/app/jobs/job-detail/job-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { User } from 'src/app/_models/user.model';
 import { AccountService } from 'src/app/_services/account.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -6,8 +6,9 @@ import { Job } from 'src/app/_models/job.model';
 import { JobService } from 'src/app/_services/job.service';
 import { ActivatedRoute } from '@angular/router';
 import { HttpEventType } from '@angular/common/http';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
-import { ProgressStatus, ProgressStatusEnum  } from 'src/app/_models/progress-status.model';
 import { CandidateService } from 'src/app/_services/candidate.service';
 import { ToastrService } from 'ngx-toastr';
 
@@ -16,14 +17,18 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './job-detail.component.html',
   styleUrls: ['./job-detail.component.css']
 })
-export class JobDetailComponent implements OnInit {
+export class JobDetailComponent implements OnInit, OnDestroy {
   currentUser: User;
   job: Job;
   id: number;
-
-  fileToUpload: File = null;
   public progress: number;
   public message: string;
+  loading: boolean = false;
+  submitted: boolean = false;
+
+  fileToUpload: File = null;
+  subscriptionId: Subscription;
+  resumeForm: FormGroup;
 
   @Output() public onUploadFinished = new EventEmitter();
 
@@ -33,7 +38,8 @@ export class JobDetailComponent implements OnInit {
     private modalService: NgbModal,
     private route: ActivatedRoute,
     private candidateService: CandidateService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder,
   ) { 
     this.accountService.user.subscribe(x => {
       this.currentUser = x;
@@ -41,13 +47,21 @@ export class JobDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe(param=>{
+    this.subscriptionId = this.route.params.subscribe(param=>{
       this.id = param['id'];
       if(this.id){
         this.loadJobDetail()
       }
     })
+
+    this.resumeForm = this.formBuilder.group({
+      fullname: [this.currentUser.fullName, Validators.required],
+      email: [this.currentUser.email, [Validators.required, Validators.email]]
+    });
   }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.resumeForm.controls; }
 
   loadJobDetail(){
     this.jobService.getJobDetail(this.id).subscribe(res=>{
@@ -76,6 +90,32 @@ export class JobDetailComponent implements OnInit {
     },error => {
       this.toastr.error(error.error)
     })
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.resumeForm.invalid) {
+        return;
+    }
+    this.loading = true;
+
+    this.currentUser['fullName'] = this.resumeForm.controls['fullname'].value;
+    this.currentUser['email'] = this.resumeForm.controls['email'].value;
+
+    // this.accountService.update(this.currentUser).subscribe(response => {
+    //   if(response)  {
+    //     this.toastr.success('Your profile has been updated successfully');
+    //     this.router.navigate(['../back'], { relativeTo: this.route });
+    //   }
+    // },error => {
+    //   this.toastr.error(error.error)
+    //   this.loading = false;
+    // })
+  }
+
+  ngOnDestroy(){
+    this.subscriptionId.unsubscribe();
   }
 
 }
